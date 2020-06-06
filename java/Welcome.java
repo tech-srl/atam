fe8d842fd81666461fc95ff966f8a58e461a353f
Eran Yahav <yahave@gmail.com>
2020-06-06 21:59:27 +0300

    welcome java

---
+++ java/Welcome.java
@@ -0,0 +1,9 @@
+public class Welcome { 
+	String prompt; 
+	public Welcome(String prompt) {
+		this.prompt = prompt;
+	} 
+	public void show() {
+		System.out.println(prompt);	
+	}
+}
---
+++ java/WelcomeMain.java
@@ -0,0 +1,6 @@
+public class WelcomeMain {
+	public static void main(String[] args) {
+		Welcome w = new Welcome("Hello World!");
+		w.show();
+	}
+}
