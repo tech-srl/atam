ed8582064b451bb832b24dea70dae0baa29e0315
Eran Yahav <yahave@gmail.com>
2020-06-06 22:57:42 +0300

    examples

---
+++ java/Add.java
@@ -0,0 +1,7 @@
+public class Add {
+	public static void main(String[] args) {
+		int x = 42;
+		int y = x + 3;
+		int z = x + 5; 
+	}
+}
---
+++ java/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+  int id;
+  String name;
+
+  public Student(int id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public void studyWith(Student another) {
+    // do some studying
+  }
+
+
+  public static void main(String[] args) {
+    Student s1 = new Student(1,"Anna");
+    Student s2 = new Student(2,"Elza");
+    s1.studyWith(s2);
+  }
+
+}
