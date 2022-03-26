b8b73b73f2ae0ed92ef9a982e4abb70597659877
Eran Yahav <yahave@gmail.com>
2022-03-26 21:16:57 +0300

    nicer hex in print bytes

---
+++ print-bytes.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+	long x = 0xCAFEBABEDEADBEEF;
+	unsigned char *p = (unsigned char*)(&x);
+	for (int i = 0; i < sizeof (x); i++)
+	{
+		printf ("%x ", *p++);
+	}
+	return 0;
+}
